8d302195c3464ca1e40ee22a9c43531f03099e88
Paulo DAlberti <phdalberti@gmail.com>
2019-07-15 14:25:29 +0900

    nested responses routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :responses
-  resources :discussions
+  resources :discussions do
+    resources :responses, shallow: true
+  end
   resources :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
